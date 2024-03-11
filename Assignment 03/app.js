1a598f3792b759dbe234c4d088cbdfa2ce41e70d
Muzaffar Ali Kalwar <160321216+MUZAFFARALIKALWAR@users.noreply.github.com>
2024-03-11 12:45:46 +0500

    Update app.js

--- Assignment 03/app.js
+++ Assignment 03/app.js
@@ -1,5 +1,5 @@
 // 3.Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-var personName = "muzaffar";
+let personName = "muzaffar";
 console.log("Uppercase: ", personName.toLocaleUpperCase());
 console.log("Lowercase: ", personName.toLowerCase());
 console.log("Titlecase: ", personName.charAt(0).toUpperCase() + personName.slice(1).toLocaleLowerCase());
