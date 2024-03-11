bff873da47f82b65e11d9159204712479180ed57
Muzafar Ali <[valid-email]>
2024-03-11 12:43:16 +0500

    first commit

---
+++ Assignment 1and 2/main.js
@@ -0,0 +1,4 @@
+// 1.Install Node.js, TypeScript and VS Code on your computer.
+// 2.Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+var personName = "Muzaffar";
+console.log("Hello ".concat(personName, ",  would you like to learn some Python today? "));
